9237727ca50c73db8c01d80f8ee5f5136414d434
mitraabhimanyu2011 <82744734+mitraabhimanyu2011@users.noreply.github.com>
2021-12-19 17:34:28 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+avengers = "";
+starWars = "";
+
+function preload()
+{
+    avengers = loadSound("music.mp3");
+    starWars = loadSound("music.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
