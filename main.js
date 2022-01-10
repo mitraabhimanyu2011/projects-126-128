7c1c98c94d8cc206166bb597e13921671f576ece
mitraabhimanyu2011 <82744734+mitraabhimanyu2011@users.noreply.github.com>
2022-01-09 18:02:08 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
+scoreLeftWrist = 0;
+leftWristY_divide_1000 = 0;
+
 function preload()
 {
     avengers = loadSound("avengers_themeMusic.mp3");
@@ -27,13 +30,22 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 600, 500);
+
+    avengers_status = avengers.isPlaying();
+    fill("FF0000");
+    stroke("FF0000");
+    if(scoreLeftWrist > 0.2)
+    {
+    circle(leftWristX, leftWristY, 20);
+    starWars.stop();
+        if(avengers_status == false)
+        {
+            avengers.play();
+            document.getElementById("song").innerHTML = "Avengers Theme Music";
+        }
+    }
 }
 
-function play()
-{
-    avengers.play();
-    avengers.setVolume(1);
-}
 function modelLoaded()
 {
     console.log("PoseNet is initialized!");
@@ -44,6 +56,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
